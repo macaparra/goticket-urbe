4ed4fa5f53ad59c86177b72dcb390a68422f19f7
Macarena <parrameca@gmail.com>
2022-11-24 01:27:35 -0400

    Now using MySQL2 libreries

--- routes/db-config.js
+++ routes/db-config.js
@@ -1,11 +1,12 @@
 // database config
-const sql = require("mysql");
+const sql = require("mysql2");
 const dotenv = require("dotenv").config();
 const db = sql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT
 });
 
 module.exports = db;
